8ad441b4427d268404c4e3ef8081162639a08cfc
adityabhosle-1 <68443991+adityabhosle-1@users.noreply.github.com>
2021-05-10 15:04:56 +0530

    Add files via upload

---
+++ q1.js
@@ -0,0 +1,127 @@
+// Creating an array and passing the number, questions, options and answers
+
+let questions = [
+   {
+     numb: 1,
+     question: "What is Magneto's real name?",
+     answer: "Max Eisenhardt",
+     options: [
+         "Matian Eisenhardt",
+         "Max Eisenhardt",
+         "Jakob Eisenhardt",
+         "Magnus Eisenhardt"
+     ]
+
+   },
+   {
+     numb: 2,
+     question: "The group of teenagers with special ability is known as?",
+     answer: "The New Mutants",
+     options: [
+         "The Gifted Mutants",
+         "The Class of Mutants",
+         "The New Mutants",
+         "The Old Mutants"
+     ]
+
+   },
+   {
+     numb: 3,
+     question: "What name is Wolverine also known by?",
+     answer: "Logan",
+     options: [
+         "Logan",
+         "Gogan",
+         "Dogan",
+         "Hogan"
+     ]
+
+   },
+   {
+     numb: 4,
+     question: "What does the special eyewear do for Cyclops?",
+     answer: "Control the energy beams",
+     options: [
+         "Transform the energy beams",
+         "Enhances the energy beams",
+         "Disable the energy beams",
+         "Control the energy beams"
+     ]
+
+   },
+   {
+     numb: 5,
+     question: "What is the name of the school that Professor X bet up for young Mutants?",
+     answer: "The Xavier's School for Gifted Youngsters",
+     options: [
+         "The Xavier's School for Mutants",
+         "The Xavier's School for Gifted Youngsters",
+         "Xavier Mutant protection program",
+         "Xavier gifted youngster protection school"
+     ]
+
+   },
+   {
+     numb: 6,
+     question: "Which X-Men did Ian McKellen play?",
+     answer: "Magneto",
+     options: [
+         "Quicksilver",
+         "Professor Xavier",
+         "Night crowler",
+         "Magneto"
+     ]
+
+   },
+   {
+     numb: 7,
+     question: "What is the name of X-Men character that can freeze anything?",
+     answer: "Ice-Man",
+     options: [
+         "Frost",
+         "Ice-Man",
+         "Professor Freeze",
+         "Caliban"
+     ]
+
+   },
+   {
+     numb: 8,
+     question: "Who is the strongest X-Men Character?",
+     answer: "Jean Grey",
+     options: [
+         "Jean Grey",
+         "Apocalypse",
+         "Jin man",
+         "Steely"
+     ]
+
+   },
+   {
+     numb: 9,
+     question: "Which X-Men did Halle Berry play?",
+     answer: "Storm",
+     options: [
+         "Riptide",
+         "Psylocke",
+         "Jean Gey",
+         "Storm"
+     ]
+
+   },
+   {
+     numb: 10,
+     question: "What did Magneto try and get Jason to do when he entered?",
+     answer: "Kill all the Humans",
+     options: [
+         "Kill all the X-mens",
+         "Kill the president of United States",
+         "Kill all the Humans",
+         "Destroy Cerebro"
+     ]
+
+   }
+
+
+
+];
